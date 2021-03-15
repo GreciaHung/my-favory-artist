37ebaaab167c1620ec4de360333feaafc3747aa7
Grecia Hung <grecia.hung.a@gmail.com>
2021-03-15 18:59:06 -0500

    delete imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,6 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app.routing';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { DataRequestsInterceptor } from './shared/interceptors/data-requests.interceptor';
 
@@ -16,8 +14,6 @@ import { DataRequestsInterceptor } from './shared/interceptors/data-requests.int
   ],
   imports: [
     BrowserModule,
-    MatIconModule,
-    MatButtonModule,
     BrowserAnimationsModule,
     AppRoutingModule,
     HttpClientModule
