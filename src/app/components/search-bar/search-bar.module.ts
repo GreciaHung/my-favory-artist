07315a1f62f25d606b40fe399319be3298595450
Grecia Hung <grecia.hung.a@gmail.com>
2021-03-14 22:13:42 -0500

    styler search bar

--- src/app/components/search-bar/search-bar.module.ts
+++ src/app/components/search-bar/search-bar.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SearchBarComponent } from './search-bar/search-bar.component';
-
-
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [SearchBarComponent],
   exports: [SearchBarComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MatInputModule,
+    MatButtonModule
   ]
 })
 export class SearchBarModule { }
