55c1b5920967ad6cfc39e46e099a415d8839e33f
Grecia.Hung <grecia.hung.a@gmail.com>
2021-03-15 19:16:15 -0430

    module: artist card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app.routing';
 import { AppComponent } from './app.component';
-import { ArtistCardComponent } from './components/artist-card/artist-card.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatIconModule } from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ArtistCardComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatIconModule,
-    MatButtonModule,
     AppRoutingModule,
     BrowserAnimationsModule
   ],
