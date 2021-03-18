1e174b2df9616b76fcfbc4067a6740390abb26f2
Grecia.Hung <grecia.hung.a@gmail.com>
2021-03-17 21:35:10 -0430

    url artist album

--- src/app/shared/global-variables.ts
+++ src/app/shared/global-variables.ts
@@ -7,5 +7,5 @@ export const END_POINTS_URLS = {
   artist_search: 'artist.search',
   artist_artists_get: 'chart.artists.get',
   artist_get: 'artist.get',
-  album_get: 'album.get'
+  album_get: 'artist.albums.get'
 }
